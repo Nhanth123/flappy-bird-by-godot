7d30f8ce0e803b8fd0ea66a5cc3c3a0c3a718715
nhan.trinh <hongnhan50@gmail.com>
2024-06-14 17:04:11 +0700

    Fix: adjust the code to capture score.

--- scripts/world.gd
+++ scripts/world.gd
@@ -7,16 +7,18 @@ var highscore: int = 0
 @onready var hud = $HUD
 
 func _ready():
-	#obstacle_spawner.connect("obstacle_created", _on_obstacle_created)
+	print("score at ready method: " + str(score))
+	obstacle_spawner.connect("obstacle_created", _on_obstacle_created)
 	hud.update_score(0)
 
 func set_score(new_score):
 	score = new_score
-	print("score: " + str(score))
+	print("Got score: " + str(score))
 	hud.update_score(score)
 
 func player_score():
 	score += 1
+	print(score)
 
-#func _on_obstacle_created(obs):
-	#obs.connect("obs", player_score)
+func _on_obstacle_created(obs):
+	obs.connect("score", player_score)

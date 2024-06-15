70acffe169ad8741b46ddf7082fa34d34fe19e19
nhan.trinh <hongnhan50@gmail.com>
2024-06-15 19:38:11 +0700

    Feature: Implement game over.

--- scripts/world.gd
+++ scripts/world.gd
@@ -5,20 +5,37 @@ var highscore: int = 0
 
 @onready var obstacle_spawner = $ObstacleSpawner
 @onready var hud = $HUD
+@onready var ground = $Ground
 
 func _ready():
-	print("score at ready method: " + str(score))
 	obstacle_spawner.connect("obstacle_created", _on_obstacle_created)
 	hud.update_score(0)
+	new_game()
 
 func set_score(new_score):
 	score = new_score
-	print("Got score: " + str(score))
 	hud.update_score(score)
 
 func player_score():
 	score += 1
-	print(score)
+	set_score(score)
 
 func _on_obstacle_created(obs):
 	obs.connect("score", player_score)
+
+func new_game():
+	score = 0
+	obstacle_spawner.start()
+
+func _on_deathzone_body_entered(body):
+	if body is Player:
+		if body.has_method("die"):
+			body.die()
+
+func _on_player_died():
+	game_over()
+
+func game_over():
+	obstacle_spawner.stop()
+	ground.get_node("AnimationPlayer").stop()
+	get_tree().call_group("obstacles", "set_physics_process", false)
